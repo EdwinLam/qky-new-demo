f3895fd2aebc114d35f1454f6c13422f8dff2306
Edwin <edwin13@qq.com>
2018-12-10 16:41:19 +0800

    增加了人脸识别

---
+++ qky-classgroup-service/src/main/java/cn/thinkjoy/arcsoft/face/enums/DetectMode.java
@@ -0,0 +1,20 @@
+package cn.thinkjoy.arcsoft.face.enums;
+
+/**
+ * 人脸检测模式常量
+ *
+ * @author Jastar·Wang
+ * @email jastar_wang@163.com
+ * @date 2018-12-4
+ * @since 2.0
+ */
+public interface DetectMode {
+	/**
+	 * 视频模式
+	 */
+	int ASF_DETECT_MODE_VIDEO = 0x00000000;
+	/**
+	 * 图片模式
+	 */
+	int ASF_DETECT_MODE_IMAGE = 0xFFFFFFFF;
+}
---
+++ qky-classgroup-service/src/main/java/cn/thinkjoy/arcsoft/face/util/ConfUtil.java
@@ -0,0 +1,19 @@
+package cn.thinkjoy.arcsoft.face.util;
+
+/**
+ * 配置
+ *
+ * @author Jastar Wang
+ * @version 1.0
+ * @date 2018/11/30
+ */
+public class ConfUtil {
+
+    // 有效期至：2019-11-16
+    public static final String appId = "GTSaeKdSqf633HNt4HhytLXjgUeNiZxa5rgqb5CmGwpU";
+    // Windows(X64)
+    public static final String appKey = "CzLuLGfDQUyN82vhrn13HFQSoC2qowc6YSiNYZpQvGGd";
+    // Windows(X86)
+    // public static final String appKey = "6K2JhygXW2hCcr6XCRtKxfqcJvXfyc6uLKDzJCvmov8D";
+
+}
